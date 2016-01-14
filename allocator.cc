267d25c345c6896eb3f9b7b501e5d2233952377a
cc509a <cc509a@gmail.com>
2016-01-14 21:06:41 +0800

    update allocator.cc
    Signed-off-by: cc509a <cc509a@gmail.com>

--- allocator.cc
+++ allocator.cc
@@ -2,6 +2,14 @@
 #define AYU_ALLOCATOR_CC
 #include "allocator.h"
 
+template <typename T, size_t BLockSize>
+inline typename Alloctator<T, BLockSize>::size_type
+Alloctator<T, BLockSize>::padPointer(data_pointer_p, size_type align)
+const noexcept
+{
+  uintptr_t result = reinterpret_cast<uintptr_t>(p);
+  return ((align - result) % align);
+}
 
 
 #endif
